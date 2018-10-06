d2af3f5772025004ad54e5efb525219eadd3858f
Felice Forby <feliceforby@gmail.com>
2018-10-06 00:19:29 +0000

    add validations for post and author

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"], message: "%{value} is not a valid category" }
+  validate :non_clickbait
+
+  def non_clickbait
+    clickbait = ["Secret", "Top", "Guess"]
+    words_in_title = self.title.split
+    words_in_title.include?(clickbait)
+  end
 end
